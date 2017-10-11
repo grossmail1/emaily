fd48475dbb6de9d3d10420f27d822f9cfb772ec0
Andrew Gross <grossmail1@gmail.com>
2017-10-11 08:12:36 -0600

    adding reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
+class App extends Component {
 
-const App = () => {
-	return (
-		<div>
-			Hey babies
-		</div>
-	)
+	componentDidMount () {
+		var iframe = this.iframe
+		axios.get('./007.xhtml').then(data => {
+			var documentText = data.data
+			var parser = new DOMParser()
+			var newDoc = parser.parseFromString(documentText, 'text/html')
+			var newNode = newDoc.documentElement
+			console.log('new doc', documentText)
+
+			var head = newDoc.querySelector('head')
+			var base = document.createElement('base')
+			base.setAttribute('href', './007.xhtml')
+			head.insertBefore(base, head.firstElementChild)
+
+			var style = document.createElement('style')
+			style.innerHTML = 'html{transform: scale(' + 1 +';transform-origin: 0 0; overflow: hidden;}'
+			head.appendChild(style)
+			iframe.contentWindow.document.open()
+			iframe.contentWindow.document.write(newNode.outerHTML)
+			iframe.contentWindow.document.close()
+		}, 'text')
+	}
+
+	render() {
+		return (
+			<div>
+				<div style={{height: '775px', width: '641.919px'}}>
+					<iframe ref={i => this.iframe = i}/>
+				</div>
+			</div>
+		)
+	}
+
 }
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-
+import reducers from './reducers'
 import App from './components/App'
 
-const store = createStore(() => [], {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware())
 
 ReactDom.render(
 	<Provider store={store}>
